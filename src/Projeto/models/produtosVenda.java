ab040b8391d78d55d8d3468bef48f128362acf27
Vinicius Donovan <105085064+ViniciusDonovanC@users.noreply.github.com>
2025-06-05 14:29:01 -0300

    Upd8 - 3
    Adicionada model produtosVenda

---
+++ src/Projeto/models/produtosVenda.java
@@ -0,0 +1,41 @@
+package Projeto.models;
+
+public class produtosVenda {
+    private String cnpjFarmacia;
+    private int idProduto;
+    private int qtdProduto;
+
+    private double valorProduto;
+
+    public String getCnpjFarmacia() {
+        return cnpjFarmacia;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public double getQtdProduto() {
+        return qtdProduto;
+    }
+
+    public double getValorProduto() {
+        return valorProduto;
+    }
+
+    public void setCnpjFarmacia(String cnpjFarmacia) {
+        this.cnpjFarmacia = cnpjFarmacia;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public void setQtdProduto(int qtdProduto) {
+        this.qtdProduto = qtdProduto;
+    }
+
+    public void setValorProduto(double valorProduto) {
+        this.valorProduto = valorProduto;
+    }
+}
