25c565ad9d48c1b4429dd429487e2e6b20e915e3
ViniciusACosta01 <viniciuscambe@hotmail.com>
2025-06-05 12:55:41 -0300

    Produto e Venda

---
+++ src/projeto/Venda.java
@@ -0,0 +1,60 @@
+package projeto;
+
+public class Venda {
+    private String cnpjFarmacia;
+    private int idVendaProduto;
+    private int idVenda;
+    private int idFuncionario;
+    
+    
+    public Venda() {
+        
+        this.idVendaProduto = 0;
+        this.idVenda = 0;
+        this.idFuncionario = 0;
+        
+    }
+    
+    public String getcnpjFarmacia() {
+        
+        return cnpjFarmacia;
+    
+    }
+    
+    public void setIdVendaProduto(int idVendaProduto) {
+        
+        this.idVendaProduto = idVendaProduto;
+
+    }
+
+    public int getIdVendaProduto() {
+        
+        return idVendaProduto;
+
+    }
+
+    public void setIdVenda(int idVenda) {
+        
+        this.idVenda = idVenda;
+
+    }
+    public int getIdVenda() {
+        
+        return idVenda;
+
+    }
+
+    public void setIdFuncionario(int idFuncionario) {
+        
+        this.idFuncionario = idFuncionario;
+
+    }
+
+    public int getIdFuncionario() {
+        
+        return idFuncionario;
+
+    }
+
+
+}
