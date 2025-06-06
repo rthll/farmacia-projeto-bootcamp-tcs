163c5b987582401501b0286f2b2395d148457133
MoisesCaris <moises.caris@edu.unifil.br>
2025-06-06 12:16:05 -0300

    Criando controller setor

--- src/Projeto/controllers/SetorCtrl.java
+++ src/Projeto/controllers/SetorCtrl.java
@@ -1,5 +1,22 @@
-package controllers;
+package Projeto.controllers;
+
+import Projeto.models.Setor;
 
 public class SetorCtrl {
-    
+
+    SessaoDAO dao;
+
+    public SetorCtrl() {
+        dao = new SessaoDAO;
+    }
+
+    public void cadastrarSetor(String nomeSetor, double valeTransporte, double valeRefeicao, double valeAlimentacao, double planoSaude) {
+        try {
+            Setor setor = new Setor(Sessao.getCnpjLogado(),nomeSetor,valeTransporte,  valeRefeicao,  valeAlimentacao, planoSaude);
+            dao.criarSetor(setor);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
